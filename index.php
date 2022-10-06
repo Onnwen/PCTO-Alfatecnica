11ee4304cb1e9f959b43acbd77bd45c1027c0c2d
TheCourierNV <andreascart04@gmail.com>
2022-10-06 09:09:47 +0200

    Login: Sistemato il login per chi non ha HttpsEverywhere
    C'era un singolo link nella pagina di login che era http invece che
    https

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ if(isset($_SESSION['session_id'])){
     <script src="https://code.jquery.com/jquery-3.6.0.js"
         integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
         crossorigin="anonymous"></script>
-    <script src="http://www.myersdaily.org/joseph/javascript/md5.js"></script>
+    <script src="https://www.myersdaily.org/joseph/javascript/md5.js"></script>
     <title>Alfatecnica - Login</title>
     <link rel="icon" href="img/logo.png">
     <style>
