394ce4882beafb513f98a40509086d5b65619ebe
Chrisis2004 <christian.canossa04@gmail.com>
2022-10-07 21:20:48 +0200

    prova della nuova query in viewplan

--- php/viewPlan.php
+++ php/viewPlan.php
@@ -1,38 +1,37 @@
 <?php
 require_once('connessione.php');
 
-if(!$idAnag == 0){
-  $select = "SELECT Sold_Products.sold_product_id, Product_Category.name, Sold_Products.x, Sold_Products.y, Product_Category.icon_image_path AS pathProd, Companies.planimetry_image_url AS pathSfondo, planimetrie.width, planimetrie.height
-             FROM app JOIN planimetrie JOIN prodotti_img JOIN prodotti JOIN anagrafica
-             ON planimetrie.id = app.idPlanimetria
-             AND app.idAnagrafica = anagrafica.id
-             AND app.idProdotto = prodotti.id
-             AND prodotti.idProdImg = prodotti_img.id
-             WHERE app.idAnagrafica = " . $idAnag;
-  $result = $pdo->query($select);
-  $arr = array();
-  $i = 0;
-  if($result){
-    while($row = $result->fetch(PDO::FETCH_ASSOC)){
-      $arr[$i] = array(
-        'id_prod' => $row['id'],
-        'nome_prod' => $row['nome_prodotto'],
-        'posX' => $row['pos_x'],
-        'posY' => $row['pos_y'],
-        'pathProd' => $row['pathProd'],
-        'pathSfondo' => $row['pathSfondo'],
-        'w' => $row['width'],
-        'h' => $row['height']
-      );
-      $i++;
+$idAnag = isset($_POST['idAnag']) ? $_POST['idAnag'] : 0;
+if (!$idAnag == 0) {
+    $select = "SELECT Sold_Products.sold_product_id AS id, Product_Category.name AS nome_prodotto, Sold_Products.x AS pos_x, Sold_Products.y AS pos_y,Companies.planimetry_image_url AS pathSfondo, Product_Category.icon_image_path AS pathProd, Companies.planimetry_image_width AS width,Companies.planimetry_image_height AS height
+              FROM Sold_Products
+              INNER JOIN Companies on Sold_Products.company_id = Companies.id
+              INNER JOIN Product_Category on Product_Category.product_category_id = Sold_Products.product_category_id
+              WHERE Companies.id = " . $idAnag;
+    $result = $pdo->query($select);
+    $arr = array();
+    $i = 0;
+    if ($result) {
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            $arr[$i] = array(
+                'id_prod' => $row['id'],
+                'nome_prod' => $row['nome_prodotto'],
+                'posX' => $row['pos_x'],
+                'posY' => $row['pos_y'],
+                'pathProd' => $row['pathProd'],
+                'pathSfondo' => $row['pathSfondo'],
+                'w' => $row['width'],
+                'h' => $row['height']
+            );
+            $i++;
+        }
+    } else {
+        $arr = [
+            'dati' => 'Nessun dato trovato',
+            'msg' => 'Planimetria ancora da configurare'
+        ];
     }
-  } else {
-    $arr = [
-      'dati' => 'Nessun dato trovato',
-      'msg' => 'Planimetria ancora da configurare'
-    ];
-  }
-  $json = json_encode($arr);
-  echo $json;
+    $json = json_encode($arr);
+    echo $json;
 }
- ?>
+?>
