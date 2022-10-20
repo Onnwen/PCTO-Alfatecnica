99b15549f5778a7f1fd6e7e1a7144b3b3b11c252
Onnwen <onnwen.cassitto@icloud.com>
2022-10-20 20:32:38 +0200

    Implementazione navbar univoca
    Co-Authored-By: TheCourierNV <58661940+TheCourierNV@users.noreply.github.com>

--- pages/dettaglio-anagrafica.php
+++ pages/dettaglio-anagrafica.php
@@ -61,36 +61,7 @@ if(isset($_SESSION['session_id'])){
         </head>
 
         <body>
-        <!-- Navbar -->
-        <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
-            <div class="container-fluid">
-                <a href="#"><img src="../img/logo.png" width="55px" height="50px"></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                        aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse header" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link" href="lista-anagrafica.php">Anagrafica</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Revisione</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Prodotti</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Utenti</a>
-                        </li>
-                        </li>
-                    </ul>
-                    <button type="button" class="btn btn-outline-danger">Logout</button>
-                </div>
-            </div>
-        </nav>
-        <!-- Fine -->
+        <?php require_once("navbar.php"); ?>
 
         <!-- Cards -->
         <div class="container">
