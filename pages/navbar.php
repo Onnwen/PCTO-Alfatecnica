f3851f28b8837aaf82e3e6e8a94811359d84149c
Onnwen <onnwen.cassitto@icloud.com>
2022-10-23 12:47:07 +0200

    Correzione minore dei percorsi

--- pages/navbar.php
+++ pages/navbar.php
@@ -1,7 +1,7 @@
 <?php
 echo '<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
     <div class="container-fluid">
-        <a href="lista-anagrafica.php"><img src="../img/logo.png" width="55px" height="50px"></a>
+        <a href="../index.php"><img src="../img/logo.png" width="55px" height="50px"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
@@ -10,7 +10,7 @@ echo '<nav class="navbar navbar-expand-lg navbar-light" style="background-color:
         <div class="collapse navbar-collapse header" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" href="dettaglio-anagrafica.php">Anagrafica</a>
+                    <a class="nav-link" href="lista-anagrafica.php">Anagrafica</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#" style="color: red">Revisione</a>
