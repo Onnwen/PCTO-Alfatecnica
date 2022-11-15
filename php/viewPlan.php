f7fea722d381be2b58c12ef5ce5e7875778b1541
Chrisis2004 <christian.canossa04@gmail.com>
2022-11-15 18:35:57 +0100

    funzionalità in dettalgio anagrafica

--- php/viewPlan.php
+++ php/viewPlan.php
@@ -3,7 +3,7 @@ require_once('connessione.php');
 
 $idAnag = isset($_POST['idAnag']) ? $_POST['idAnag'] : 0;
 if (!$idAnag == 0) {
-    $select = "SELECT Sold_Products.sold_product_id AS id, Product_Category.name AS nome_prodotto, Sold_Products.x AS pos_x, Sold_Products.y AS pos_y,Companies.planimetry_image_url AS pathSfondo, Product_Category.icon_image_path AS pathProd, Companies.planimetry_image_width AS width,Companies.planimetry_image_height AS height
+    $select = "SELECT Sold_Products.sold_product_id AS id, Product_Category.name AS nome_prodotto, Product_Category.product_category_id as id_Categoria, Sold_Products.x AS pos_x, Sold_Products.y AS pos_y,Companies.planimetry_image_url AS pathSfondo, Product_Category.icon_image_path AS pathProd, Companies.planimetry_image_width AS width,Companies.planimetry_image_height AS height
               FROM Sold_Products
               INNER JOIN Companies on Sold_Products.company_id = Companies.id
               INNER JOIN Product_Category on Product_Category.product_category_id = Sold_Products.product_category_id
@@ -16,6 +16,7 @@ if (!$idAnag == 0) {
             $arr[$i] = array(
                 'id_prod' => $row['id'],
                 'nome_prod' => $row['nome_prodotto'],
+                'id_Categoria' => $row['id_Categoria'],
                 'posX' => $row['pos_x'],
                 'posY' => $row['pos_y'],
                 'pathProd' => $row['pathProd'],
