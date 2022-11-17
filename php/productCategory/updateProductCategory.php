5cd0df0acf333ca14b4f31ce5e7985eb1d3afd4a
Onnwen <onnwen.cassitto@icloud.com>
2022-11-17 09:49:36 +0100

    Risoluzione bug

--- php/productCategory/updateProductCategory.php
+++ php/productCategory/updateProductCategory.php
@@ -8,23 +8,24 @@ try {
 
     $updateFieldIndex = 0;
     while ($_POST[$updateFieldIndex . "updateFieldId"] !== null) {
-        $pdo->query("UPDATE Product_Fields SET name = '" . $_POST[$updateFieldIndex . "updateFieldName"] . "' WHERE field_id = '" . $_POST[$updateFieldIndex . "updateFieldId"] . "')");
+        $pdo->query("UPDATE Product_Fields SET name = '" . $_POST[$updateFieldIndex . "updateFieldName"] . "' WHERE field_id = '" . $_POST[$updateFieldIndex . "updateFieldId"] . "';");
         $updateFieldIndex++;
     }
 
     $newFieldIndex = 0;
-    while ($_POST[$newFieldIndex . "newFieldId"] !== null) {
-        $pdo->query("INSERT INTO Product_Fields (product_category_id, name) VALUES ('" . $productCategoryId. "', '" . $_POST[$newFieldIndex . "newFieldName"] . "')");
+    while ($_POST[$newFieldIndex . "newFieldName"] !== null) {
+        $pdo->query("INSERT INTO Product_Fields (product_category_id, name) VALUES ('" . $productCategoryId. "', '" . $_POST[$newFieldIndex . "newFieldName"] . "');");
         $newFieldIndex++;
     }
 
     $deleteFieldIndex = 0;
     while ($_POST[$deleteFieldIndex . "deleteFieldId"] !== null) {
-        $pdo->query("DELETE FROM Product_Fields WHERE field_id = '" . $_POST[$deleteFieldIndex . "deleteFieldId"] . "')");
+        $pdo->query("DELETE FROM Product_Fields WHERE field_id = '" . $_POST[$deleteFieldIndex . "deleteFieldId"] . "';");
         $deleteFieldIndex++;
     }
 
     $pdo->commit();
+    echo "success";
 } catch (Exception $e) {
     echo $e;
     http_response_code(400);
