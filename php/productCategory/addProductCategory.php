ed02b177f523006f877b003be9937fa447cb482b
Onnwen <onnwen.cassitto@icloud.com>
2022-11-17 18:02:25 +0100

    Risolto bug che impediva il caricamento di formulari

--- php/productCategory/addProductCategory.php
+++ php/productCategory/addProductCategory.php
@@ -7,34 +7,38 @@ $iconPath = (isset($_POST["icon_path"]) ? $_POST('icon_path') : "");
 
 try {
     $pdo->beginTransaction();
-    $pdo->query("INSERT INTO Product_Category (name, visualization_type, icon_image_path) VALUES ('" . $productCategoryName . "', 0, '" . $iconPath . "')");
+    $pdo->query("INSERT INTO Product_Category (name, visualization_type, icon_image_path) VALUES ('" . $productCategoryName . "', '". $visualizationType . "', '" . $iconPath . "')");
 
     $newProductCategoryId = $pdo->lastInsertId();
+    $pdo->commit();
 
     if ($visualizationType == 0) {
         $fieldId = 0;
+        $pdo->beginTransaction();
         while ($_POST[$fieldId . "input"] !== null) {
             $pdo->query("INSERT INTO Product_Fields (product_category_id, name) VALUES ('" . $newProductCategoryId . "', '" . $_POST[$fieldId . "input"] . "')");
             $fieldId++;
         }
+        $pdo->commit();
     }
     else {
         $sectionId = 0;
         while ($_POST[$sectionId . "sectionName"] !== null) {
+            $pdo->beginTransaction();
             $pdo->query("INSERT INTO Form_Sections (name) VALUES ('" . $_POST[$sectionId . "sectionName"] . "')");
             $newSectionId = $pdo->lastInsertId();
-            echo $newSectionId;
+            $pdo->commit();
 
             $fieldId = 0;
+            $pdo->beginTransaction();
             while ($_POST[$sectionId . $fieldId . "fieldName"] !== null) {
-                $pdo->query("INSERT INTO Form_Fields (product_category_id, question, section_id) VALUES ('" . $newProductCategoryId . "', '" . $_POST[$sectionId . $fieldId . "fieldName"] . "', '" . $sectionId . "')");
+                $pdo->query("INSERT INTO Form_Fields (product_category_id, question, section_id) VALUES ('" . $newProductCategoryId . "', '" . $_POST[$sectionId . $fieldId . "fieldName"] . "', '" . $newSectionId . "')");
                 $fieldId++;
             }
+            $pdo->commit();
             $sectionId++;
         }
     }
-
-    $pdo->commit();
 } catch (Exception $e) {
     echo $e;
     http_response_code(400);
