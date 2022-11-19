a8291a5f9f5a87154eee3dfe135aa169aa2e2319
TheCourierNV <andreascart04@gmail.com>
2022-11-19 20:10:06 +0100

    Implementata durata variabile per le revisioni
    Salvo la durata della revisione in `Product_Category` e lo uso nel
    backend per calcolare la data di scadenza della revisione

--- php/getRevisions.php
+++ php/getRevisions.php
@@ -3,8 +3,7 @@ session_start();
 require_once("connessione.php");
 
 # Ritorna tutte le revisioni attuali ordinate per data di scadenza
-# TODO: Bisogna implementare la data di scadenza variabile; la Deadline non sarà sempre a 6 mesi dall'ultima revisione
-$Query = "SELECT Companies.name as CompanyName, Product_Category.name as ProductCategoryName, Tabella.LastRevision, ADD_MONTHS(Tabella.LastRevision, 6) as Deadline FROM Companies, Product_Category, (SELECT Revisions.product_category_id, Revisions.company_id, MAX(DATE(Revisions.data)) as LastRevision FROM Revisions GROUP BY Revisions.company_id, Revisions.product_category_id) as Tabella WHERE Tabella.product_category_id = Product_Category.product_category_id AND Tabella.company_id = Companies.id ORDER BY Deadline ASC";
+$Query = "SELECT Companies.name as CompanyName, Product_Category.name as ProductCategoryName, Tabella.LastRevision, ADD_MONTHS(Tabella.LastRevision, Product_Category.revisionMonthDuration) as Deadline FROM Companies, Product_Category, (SELECT Revisions.product_category_id, Revisions.company_id, MAX(DATE(Revisions.data)) as LastRevision FROM Revisions GROUP BY Revisions.company_id, Revisions.product_category_id) as Tabella WHERE Tabella.product_category_id = Product_Category.product_category_id AND Tabella.company_id = Companies.id ORDER BY Deadline ASC";
 
 $Result = $pdo->query(($Query));
 
