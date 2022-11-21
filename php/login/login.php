19a1fff75df78783d02c71fcfa0a3f3dab700751
Chrisis2004 <92294370+Chrisis2004@users.noreply.github.com>
2022-11-21 22:21:55 +0100

    hashing pasword
    sorry about that

--- php/login/login.php
+++ php/login/login.php
@@ -5,24 +5,24 @@ require_once('../connessione.php');
 $email = isset($_POST['email']) ? $_POST['email'] : '';
 $pw = isset($_POST['pw']) ? $_POST['pw'] : '';
 
-$select = "SELECT email, hashed_password
+$select = "SELECT email, hashed_password AS password
            FROM Users
-           WHERE email = :email
-           AND hashed_password = :pw";
+           WHERE email = :email";
 $pre = $pdo->prepare($select);
 $pre->bindParam(':email', $email, PDO::PARAM_STR);
-$pre->bindParam(':pw', $pw, PDO::PARAM_STR);
 $pre->execute();
 $check = $pre->fetch(PDO::FETCH_ASSOC);
 if(!$check){
   echo 'userWrong';
 } else {
-  session_regenerate_id();
-  $_SESSION['session_id'] = session_id();
-  $_SESSION['session_email'] = $check['email'];
-  echo 'login';
+  if (password_verify($pw,$check['password'])){
+    session_regenerate_id();
+    $_SESSION['session_id'] = session_id();
+    $_SESSION['session_email'] = $check['email'];
+    echo 'login';
+  } else {
+    echo 'userWrong';
+  }
   exit;
 }
-
-
  ?>
