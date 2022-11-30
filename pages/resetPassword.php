35496e571a4025cb8721a52cc1a479635019ef8a
Chrisis2004 <christian.canossa04@gmail.com>
2022-11-30 10:15:30 +0100

    page of retriving password, mail of retriving

--- pages/resetPassword.php
+++ pages/resetPassword.php
@@ -26,7 +26,7 @@ if (isset($_SESSION['session_id'])) {
         <script src="https://kit.fontawesome.com/c0c3eed4d9.js" crossorigin="anonymous"></script>
         <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
         <script>
-            const idUser = <?php echo $idUser?>
+            const idUser = <?php echo $idUser?>;
             const newPassword = $('#newPassword');
             const confirmPassword = $('#confirmPassword');
             const validatePassword = (password) => {
