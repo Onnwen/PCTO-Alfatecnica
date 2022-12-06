969cdd04e4194673e238e257bb9e23bc4ba8b830
Chrisis2004 <christian.canossa04@gmail.com>
2022-12-06 23:15:20 +0100

    conferma registrazione implementazione mail e aggiustamenti alla grafica

---
+++ php/login/confirmRegistration.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once('connessione.php');
+
+$mail = isset($_POST['email']) ? $_POST['email'] : 0;
+
+if ($mail != 0) {
+    $updProd = "UPDATE Users SET active = '1' WHERE email = '" . $mail . "'";
+    $res = $pdo->query($updProd);
+    if ($res)
+        echo 1;
+    else
+        echo 0;
+}
+?>
