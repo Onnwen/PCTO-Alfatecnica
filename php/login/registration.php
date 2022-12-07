ff4593fc28d7174268555b3d634e6332abc23cc6
Chrisis2004 <christian.canossa04@gmail.com>
2022-12-07 18:55:53 +0100

    conferma registrazione implementazione mail e aggiustamenti alla grafica

--- php/login/registration.php
+++ php/login/registration.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 require_once('../connessione.php');
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 require '../../vendor/phpmailer/phpmailer/src/Exception.php';
 require '../../vendor/phpmailer/phpmailer/src/PHPMailer.php';
 require '../../vendor/phpmailer/phpmailer/src/SMTP.php';
