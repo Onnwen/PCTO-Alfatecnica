077307cf93187a95c465e643ade8c2436fe3f50f
TheCourierNV <andreascart04@gmail.com>
2022-12-16 10:23:45 +0100

    Revisioni: File di base per creare nuova revisione
    Non è ancora completo, ci sono dei `FIXME:` a riguardo, ma
    l'infarinatura di base c'è

---
+++ php/revisions/makeRevision.php
@@ -0,0 +1,18 @@
+<?php
+require_once("../connessione.php");
+
+# FIXME: Ritorna un messaggio di errore al fontend in modo che possa avvisare l'utente in modo appropriato
+$revisionedProduct = isset($_POST["product"]) ? $_POST["product"] : exit();
+$revisionedCompany = isset($_POST["company"]) ? $_POST["company"] : exit();
+$revisionDate = isset($_POST["revisionDate"]) ? $_POST["revisionDate"] : exit();
+
+# FIXME: Controlla che l'utente sia autenticato come admin
+$query = "INSERT INTO Revisions(product_category_id, company_id, data) VALUES(:product_category_id, :company_id, :data)";
+$pre = $pdo->prepare($query);
+
+$pre->bindParam(":product_category_id", $revisionedProduct, PDO::PARAM_INT);
+$pre->bindParam(":company_id" . $revisionedCompany, PDO::PARAM_STR);
+$pre->bindParam(":data", $revisionDate, PDO::PARAM_STR);
+
+$pre->execute();
+exit();
