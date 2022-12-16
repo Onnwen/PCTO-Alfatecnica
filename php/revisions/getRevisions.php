f7715974ff320eef6e6c50329fc95a869406aab7
TheCourierNV <andreascart04@gmail.com>
2022-12-16 10:42:36 +0100

    getRevisions: Ritorna gli id dell'Azienda e del prodotto
    Questo mi semplifica la vita in `lista_revisioni.php`, visto che mi
    servono gli id per poter effettuare una nuova revisione.

--- php/revisions/getRevisions.php
+++ php/revisions/getRevisions.php
@@ -3,7 +3,7 @@ session_start();
 require_once("../connessione.php");
 
 # Ritorna tutte le revisioni attuali ordinate per data di scadenza
-$Query = "SELECT Companies.name as CompanyName, Product_Category.name as ProductCategoryName, Tabella.LastRevision, ADD_MONTHS(Tabella.LastRevision, Product_Category.revisionMonthDuration) as Deadline FROM Companies, Product_Category, (SELECT Revisions.product_category_id, Revisions.company_id, MAX(DATE(Revisions.data)) as LastRevision FROM Revisions GROUP BY Revisions.company_id, Revisions.product_category_id) as Tabella WHERE Tabella.product_category_id = Product_Category.product_category_id AND Tabella.company_id = Companies.id ORDER BY Deadline ASC";
+$Query = "SELECT Companies.id as CompanyID, Companies.name as CompanyName, Product_Category.product_category_id as ProductCategoryID, Product_Category.name as ProductCategoryName, Tabella.LastRevision, ADD_MONTHS(Tabella.LastRevision, Product_Category.revisionMonthDuration) as Deadline FROM Companies, Product_Category, (SELECT Revisions.product_category_id, Revisions.company_id, MAX(DATE(Revisions.data)) as LastRevision FROM Revisions GROUP BY Revisions.company_id, Revisions.product_category_id) as Tabella WHERE Tabella.product_category_id = Product_Category.product_category_id AND Tabella.company_id = Companies.id ORDER BY Deadline ASC";
 
 $Result = $pdo->query(($Query));
 
@@ -13,7 +13,9 @@ if ($Result) {
     for ($i = 0; $row = $Result->fetch(PDO::FETCH_ASSOC); $i++) {
         $array[$i] = array(
             "CompanyName" => $row["CompanyName"],
+            "CompanyID" => $row["CompanyID"],
             "ProductCategoryName" => $row["ProductCategoryName"],
+            "ProductCategoryID" => $row["ProductCategoryID"],
             "LastRevision" => $row["LastRevision"],
             "Deadline" => $row["Deadline"]
         );
