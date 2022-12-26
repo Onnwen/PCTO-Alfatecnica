b4eaa92c4e987af9d14c81ccd5215016095e3c5a
TheCourierNV <andreascart04@gmail.com>
2022-12-26 21:09:39 +0100

    getRevisions: Fix per nuovo hosting
    Salta fuori che ADD_MONTHS esiste solo da MariaDB 10.6, mentre Debian ha
    ancora la 10.5...

--- php/revisions/getRevisions.php
+++ php/revisions/getRevisions.php
@@ -3,7 +3,7 @@ session_start();
 require_once("../connessione.php");
 
 # Ritorna tutte le revisioni attuali ordinate per data di scadenza
-$Query = "SELECT Companies.id as CompanyID, Companies.name as CompanyName, Product_Category.product_category_id as ProductCategoryID, Product_Category.name as ProductCategoryName, Tabella.LastRevision, ADD_MONTHS(Tabella.LastRevision, Product_Category.revisionMonthDuration) as Deadline FROM Companies, Product_Category, (SELECT Revisions.product_category_id, Revisions.company_id, MAX(DATE(Revisions.data)) as LastRevision FROM Revisions GROUP BY Revisions.company_id, Revisions.product_category_id) as Tabella WHERE Tabella.product_category_id = Product_Category.product_category_id AND Tabella.company_id = Companies.id ORDER BY Deadline ASC";
+$Query = "SELECT Companies.id as CompanyID, Companies.name as CompanyName, Product_Category.product_category_id as ProductCategoryID, Product_Category.name as ProductCategoryName, Tabella.LastRevision, DATE_ADD(Tabella.LastRevision, INTERVAL Product_Category.revisionMonthDuration MONTH) as Deadline FROM Companies, Product_Category, (SELECT Revisions.product_category_id, Revisions.company_id, MAX(DATE(Revisions.data)) as LastRevision FROM Revisions GROUP BY Revisions.company_id, Revisions.product_category_id) as Tabella WHERE Tabella.product_category_id = Product_Category.product_category_id AND Tabella.company_id = Companies.id ORDER BY Deadline ASC";
 
 $Result = $pdo->query(($Query));
 
