6a68eadd4f354c8e4663a27bd0df18a86a111003
TheCourierNV <andreascart04@gmail.com>
2023-01-03 21:06:56 +0100

    deleteCategory: Autenticazione + leggermente RESTful
    Non completamente restful causa POST invece di DELETE

--- php/deleteCategory.php
+++ php/deleteCategory.php
@@ -1,30 +1,39 @@
 <?php
+# FIXME: Usare DELETE invece di POST
+
 require_once('connessione.php');
+require_once("authentication/authentication.php");
 
-$idCategory = isset($_POST['idCategory']) ? $_POST['idCategory'] : 0;
-$idCompany = isset($_POST['idCompany']) ? $_POST['idCompany'] : 0;
-
-if(!$idCategory == 0 && !$idCompany==0){
-    $deleteRevisions = "DELETE FROM Revisions WHERE product_category_id = :id AND company_id = :idCompany";
-
-    $res = $pdo->prepare($deleteRevisions);
-    $res->bindParam(":id", $idCategory, PDO::PARAM_INT);
-    $res->bindParam(":idCompany", $idCompany, PDO::PARAM_INT);
-    $res->execute();
-    if($res)
-        echo 1;
-    else
-        echo 0;
-
-    $delete = "DELETE FROM Sold_Products WHERE product_category_id = :id AND company_id = :idCompany";
-    $res = $pdo->prepare($delete);
-    $res->bindParam(":id", $idCategory, PDO::PARAM_INT);
-    $res->bindParam(":idCompany", $idCompany, PDO::PARAM_INT);
-    $res->execute();
-    if($res)
-        echo 1;
-    else
-        echo 0;
-
+if (!$isAuthenticated) {
+    http_response_code(401);
+    exit();
 }
+
+if (!$isTechnician) {
+    http_response_code(403);
+    exit();
+}
+
+$idCategory = isset($_POST['idCategory']) ? $_POST['idCategory'] : null;
+$idCompany = isset($_POST['idCompany']) ? $_POST['idCompany'] : null;
+
+if (is_null($idCategory) || is_null($idCompany)) {
+    http_response_code(400);
+    exit();
+}
+
+$deleteRevisions = "DELETE FROM Revisions WHERE product_category_id = :id AND company_id = :idCompany";
+
+$res = $pdo->prepare($deleteRevisions);
+$res->bindParam(":id", $idCategory, PDO::PARAM_INT);
+$res->bindParam(":idCompany", $idCompany, PDO::PARAM_INT);
+$res->execute();
+
+$delete = "DELETE FROM Sold_Products WHERE product_category_id = :id AND company_id = :idCompany";
+$res = $pdo->prepare($delete);
+$res->bindParam(":id", $idCategory, PDO::PARAM_INT);
+$res->bindParam(":idCompany", $idCompany, PDO::PARAM_INT);
+$res->execute();
+
+exit();
 ?>
