b6546ae7cf622d142d2ae5e468883e8c30c30a5f
TheCourierNV <andreascart04@gmail.com>
2023-01-03 19:27:33 +0100

    registrationConfirm: Implementata autenticazione
    In questo caso è "al contrario", visto che si parla del processo di
    registrazione

--- pages/registrationConfirm.php
+++ pages/registrationConfirm.php
@@ -1,7 +1,8 @@
 <?php
-session_start();
 require_once('../php/connessione.php');
-if (isset($_SESSION['session_id'])) {
+require_once("../php/authentication/authentication.php");
+
+if ($isAuthenticated) {
     header('location: pages/lista-anagrafica.php');
 } else {
     $mail = $_GET['email'];
