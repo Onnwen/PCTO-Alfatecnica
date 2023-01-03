8c40de604bbb435c55c9521daf7f4acd2c0c495e
TheCourierNV <andreascart04@gmail.com>
2023-01-03 18:25:35 +0100

    makeRevision: Autenticazione fatta a dovere
    Solo i tecnici (e gli admin, in virtù di avere tutti i permessi) possono
    effettuare delle revisioni; se l'autenticazione fallisce ritorniamo i
    codici http adeguati (401 e 403)

--- php/revisions/makeRevision.php
+++ php/revisions/makeRevision.php
@@ -1,5 +1,16 @@
 <?php
 require_once("../connessione.php");
+require_once("../authentication/authentication.php");
+
+if (!$isAuthenticated) {
+    http_response_code(401);
+    exit();
+}
+
+if (!$isTechnician) {
+    http_response_code(403);
+    exit();
+}
 
 $revisionedProduct = isset($_POST["product"]) ? $_POST["product"] : "";
 $revisionedCompany = isset($_POST["company"]) ? $_POST["company"] : "";
@@ -7,10 +18,9 @@ $revisionDate = isset($_POST["revisionDate"]) ? $_POST["revisionDate"] : "";
 
 if ($revisionedCompany == "" || $revisionedProduct == "" || $revisionDate == "") {
     http_response_code(400);
-    die();
+    exit();
 }
 
-# FIXME: Controlla che l'utente sia autenticato come admin
 $query = "INSERT INTO Revisions(product_category_id, company_id, data) VALUES(:product_category_id, :company_id, :data)";
 $pre = $pdo->prepare($query);
 
@@ -19,5 +29,4 @@ $pre->bindParam(":company_id", $revisionedCompany, PDO::PARAM_INT);
 $pre->bindParam(":data", $revisionDate, PDO::PARAM_STR);
 
 $pre->execute();
-
 exit();
