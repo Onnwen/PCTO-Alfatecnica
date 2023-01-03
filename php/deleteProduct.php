2ecdb7edef3f5f03f7ad8cff544b964a771a02b8
TheCourierNV <andreascart04@gmail.com>
2023-01-03 21:15:39 +0100

    deleteProduct: Autenticazione + leggermente RESTful

--- php/deleteProduct.php
+++ php/deleteProduct.php
@@ -1,15 +1,27 @@
 <?php
+# FIXME: Usare DELETE al posto di POST
 require_once('connessione.php');
+require_once("authentication/authentication.php");
 
-$idProd = isset($_POST['id']) ? $_POST['id'] : 0;
-if(!$idProd == 0){
-  $delete = "DELETE FROM Sold_Products WHERE sold_product_id = :id";
-  $res = $pdo->prepare($delete);
-  $res->bindParam(":id", $idProd, PDO::PARAM_INT);
-  $res->execute();
-  if($res)
-    echo 1;
-  else
-    echo 0;
+if (!$isAuthenticated) {
+    http_response_code(401);
+    exit();
 }
- ?>
+
+if (!$isTechnician) {
+    http_response_code(403);
+    exit();
+}
+
+$idProd = isset($_POST['id']) ? $_POST['id'] : null;
+
+if (is_null($idProd)) {
+    http_response_code(400);
+    exit();
+}
+
+$delete = "DELETE FROM Sold_Products WHERE sold_product_id = :id";
+$res = $pdo->prepare($delete);
+$res->bindParam(":id", $idProd, PDO::PARAM_INT);
+$res->execute();
+exit();
