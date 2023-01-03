4723087bf4987bbd69c3028b499f9398c0bc5f25
TheCourierNV <andreascart04@gmail.com>
2023-01-03 17:57:30 +0100

    logout: Resetta anche le altre variabili in sessione

--- php/login/logout.php
+++ php/login/logout.php
@@ -4,5 +4,13 @@ session_start();
 if (isset($_SESSION['session_id'])) {
     unset($_SESSION['session_id']);
 }
+
+if (isset($_SESSION['session_email'])) {
+    unset($_SESSION['session_email']);
+}
+
+if (isset($_SESSION['role'])) {
+    unset($_SESSION['role']);
+}
 exit;
 ?>
