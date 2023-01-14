ea7a56b7ebc52395b06cde80ff3ce70d422942ec
Onnwen <onnwen.cassitto@icloud.com>
2023-01-14 08:42:46 +0100

    Risoluzione bug

--- php/deleteUser.php
+++ php/deleteUser.php
@@ -286,7 +286,11 @@ if ($_POST["user_id"]) {
 
     $pdo->query("DELETE FROM User_Company WHERE user_id = " . $userId . ";");
     $pdo->query("DELETE FROM Users WHERE user_id = " . $userId . ";");
+
+    echo 1;
 } else {
     http_response_code(400);
+
+    echo 0;
     exit;
 }
