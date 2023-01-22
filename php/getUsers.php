60760e3e9c325d861503bda42a27262309ad69ca
Chrisis2004 <christian.canossa04@gmail.com>
2023-01-22 16:26:45 +0100

    Sezione utenti ultimata

--- php/getUsers.php
+++ php/getUsers.php
@@ -7,35 +7,25 @@
     if($userNameSurname !== '' && $company !== ''){
         $select = "SELECT Users.user_id as id, first_name, last_name, email, role, active, activedByCompany, Companies.name AS company
                    FROM Users
-                        INNER JOIN User_Company ON User_Company.user_id = Users.user_id
-                        INNER JOIN Companies ON Companies.id = User_Company.company_id
+                        LEFT JOIN User_Company ON User_Company.user_id = Users.user_id
+                        LEFT JOIN Companies ON Companies.id = User_Company.company_id
                    WHERE first_name LIKE '%$userNameSurname%' OR last_name LIKE '%$userNameSurname%' AND Companies.name LIKE '%$company%'";
         $pre = $pdo->prepare($select);
         $pre->execute();
         $check = $pre->fetchAll(PDO::FETCH_ASSOC);
-        if(!$check){
-            echo "error";
-            exit;
-        } else {
-            echo json_encode($check);
-            exit;
-        }
+        echo json_encode($check);
+        exit;
     } else if ($userNameSurname !== ''){
         $select = "SELECT Users.user_id, first_name, last_name, email, role, active, activedByCompany, Companies.name AS company
                    FROM Users
-                        INNER JOIN User_Company ON User_Company.user_id = Users.user_id
-                        INNER JOIN Companies ON Companies.id = User_Company.company_id
+                        LEFT JOIN User_Company ON User_Company.user_id = Users.user_id
+                        LEFT JOIN Companies ON Companies.id = User_Company.company_id
                    WHERE first_name LIKE '%$userNameSurname%' OR last_name LIKE '%$userNameSurname%'";
         $pre = $pdo->prepare($select);
         $pre->execute();
         $check = $pre->fetchAll(PDO::FETCH_ASSOC);
-        if(!$check){
-            echo "error";
-            exit;
-        } else {
-            echo json_encode($check);
-            exit;
-        }
+        echo json_encode($check);
+        exit;
     } else if ($company !== ''){
         $select = "SELECT Users.user_id, first_name, last_name, email, role, active, activedByCompany, Companies.name AS company
                    FROM Users
@@ -45,13 +35,8 @@
         $pre = $pdo->prepare($select);
         $pre->execute();
         $check = $pre->fetchAll(PDO::FETCH_ASSOC);
-        if(!$check){
-            echo "error";
-            exit;
-        } else {
-            echo json_encode($check);
-            exit;
-        }
+        echo json_encode($check);
+        exit;
     } else {
         $select = "SELECT Users.user_id, first_name, last_name, email, role, active, activedByCompany, Companies.name AS company
                    FROM Users
@@ -60,12 +45,7 @@
         $pre = $pdo->prepare($select);
         $pre->execute();
         $check = $pre->fetchAll(PDO::FETCH_ASSOC);
-        if(!$check){
-            echo "error";
-            exit;
-        } else {
-            echo json_encode($check);
-            exit;
-        }
+        echo json_encode($check);
+        exit;
     }
 ?>
