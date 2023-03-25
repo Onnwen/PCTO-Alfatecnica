80f3f89944ee2bd88e8ae34ee116a6bf230a5bf7
TheCourierNV <andreascart04@gmail.com>
2023-03-25 09:04:11 +0100

    getUsers: Semplificato il codice rimuovendo ripetizioni
    Invece di ripetere 4 volte la parte costante della query, adesso
    specifichiamo solo la parte che cambia.

--- php/getUsers.php
+++ php/getUsers.php
@@ -10,47 +10,20 @@ if (!$isAuthenticated) {
 $userNameSurname = isset($_POST['userNameSurname']) ? $_POST['userNameSurname'] : null;
 $company = isset($_POST['company']) ? $_POST['company'] : null;
 
+$select = "SELECT Users.user_id as id, first_name, last_name, email, role, active, activedByCompany, Companies.name AS company
+    FROM Users
+    LEFT JOIN User_Company ON User_Company.user_id = Users.user_id
+    LEFT JOIN Companies ON Companies.id = User_Company.company_id ";
+
 if (is_null($userNameSurname) && is_null($company)) {
-    $select = "SELECT Users.user_id as id, first_name, last_name, email, role, active, activedByCompany, Companies.name AS company
-                FROM Users
-                    LEFT JOIN User_Company ON User_Company.user_id = Users.user_id
-                    LEFT JOIN Companies ON Companies.id = User_Company.company_id
-                WHERE first_name LIKE '%$userNameSurname%' OR last_name LIKE '%$userNameSurname%' AND Companies.name LIKE '%$company%'";
-    $pre = $pdo->prepare($select);
-    $pre->execute();
-    $check = $pre->fetchAll(PDO::FETCH_ASSOC);
-    echo json_encode($check);
-    exit;
+    $select .= "WHERE first_name LIKE '%$userNameSurname%' OR last_name LIKE '%$userNameSurname%' AND Companies.name LIKE '%$company%'";
 } else if (is_null($userNameSurname)) {
-    $select = "SELECT Users.user_id, first_name, last_name, email, role, active, activedByCompany, Companies.name AS company
-                FROM Users
-                    LEFT JOIN User_Company ON User_Company.user_id = Users.user_id
-                    LEFT JOIN Companies ON Companies.id = User_Company.company_id
-                WHERE first_name LIKE '%$userNameSurname%' OR last_name LIKE '%$userNameSurname%'";
-    $pre = $pdo->prepare($select);
-    $pre->execute();
-    $check = $pre->fetchAll(PDO::FETCH_ASSOC);
-    echo json_encode($check);
-    exit;
+    $select .= "WHERE first_name LIKE '%$userNameSurname%' OR last_name LIKE '%$userNameSurname%'";
 } else if (is_null($company)) {
-    $select = "SELECT Users.user_id, first_name, last_name, email, role, active, activedByCompany, Companies.name AS company
-                FROM Users
-                    INNER JOIN User_Company ON User_Company.user_id = Users.user_id
-                    INNER JOIN Companies ON Companies.id = User_Company.company_id
-                WHERE Companies.name LIKE '%$company%'";
-    $pre = $pdo->prepare($select);
-    $pre->execute();
-    $check = $pre->fetchAll(PDO::FETCH_ASSOC);
-    echo json_encode($check);
-    exit;
-} else {
-    $select = "SELECT Users.user_id, first_name, last_name, email, role, active, activedByCompany, Companies.name AS company
-                FROM Users
-                    LEFT JOIN User_Company ON User_Company.user_id = Users.user_id
-                    LEFT JOIN Companies ON Companies.id = User_Company.company_id";
-    $pre = $pdo->prepare($select);
-    $pre->execute();
-    $check = $pre->fetchAll(PDO::FETCH_ASSOC);
-    echo json_encode($check);
-    exit;
+    $select .= "WHERE Companies.name LIKE '%$company%'";
 }
+
+$pre = $pdo->prepare($select);
+$pre->execute();
+$check = $pre->fetchAll(PDO::FETCH_ASSOC);
+echo json_encode($check);
