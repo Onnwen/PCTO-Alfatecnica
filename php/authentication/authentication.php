b04ec94aea02a4719d6d5c88cecacda1f6ab6d35
TheCourierNV <andreascart04@gmail.com>
2023-03-29 11:26:10 +0200

    authentication: Aggiungi controlli per `user_id`

--- php/authentication/authentication.php
+++ php/authentication/authentication.php
@@ -5,14 +5,15 @@ session_start();
 
 $isRoleValid = isset($_SESSION['role']);
 $isEmailValid = isset($_SESSION['session_email']);
+$isUserIdValid = isset($_SESSION['user_id']);
 $isSessionValid = isset($_SESSION['session_id']);
 
-$isAuthenticated = $isRoleValid && $isEmailValid && $isSessionValid;
+$isAuthenticated = $isRoleValid && $isEmailValid && $isUserIdValid && $isSessionValid;
 
 $isTechnician = $_SESSION['role'] >= 1 && $_SESSION['role'] <= 2 && $isRoleValid;
 $isAdmin = $_SESSION['role'] == 1 && $isRoleValid;
 $isUser = $_SESSION['role'] >= 0 && $isRoleValid;
 
 $userEmail = $isEmailValid ? $_SESSION['session_email'] : "";
-
+$userId = $isUserIdValid ? $_SESSION['user_id'] : "";
 ?>
