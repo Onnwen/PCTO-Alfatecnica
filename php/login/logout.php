5ff7e51d2b6508d16483bd3d73a32db03d3d050b
TheCourierNV <andreascart04@gmail.com>
2023-03-29 11:15:02 +0200

    logout: Distruggi la sessione nel modo corretto
    Prima unsettavamo manualmente ogni singola variabile senza
    effettivamente distruggere la sessione, adesso usiamo l'API corretta.

--- php/login/logout.php
+++ php/login/logout.php
@@ -1,20 +1,8 @@
 <?php
 session_start();
 
-if (isset($_SESSION['session_id'])) {
-    unset($_SESSION['session_id']);
-}
+session_unset();
+session_destroy();
 
-if (isset($_SESSION['session_email'])) {
-    unset($_SESSION['session_email']);
-}
-
-if (isset($_SESSION['role'])) {
-    unset($_SESSION['role']);
-}
-
-if (isset($_SESSION['user_id'])) {
-    unset($_SESSION['user_id']);
-}
 exit;
 ?>
