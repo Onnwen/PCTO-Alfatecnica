31b579f8389f638fa4765d2558a89711e7906440
Chrisis2004 <christian.canossa04@gmail.com>
2023-11-27 17:09:16 +0100

    fix of a problem that for every product the program made a new revision

--- php/addProduct.php
+++ php/addProduct.php
@@ -13,6 +13,18 @@ $productFieldsQuery = $pdo->prepare($productFieldsSql);
 $productFieldsQuery->bindParam(':id', $productCategoryId, PDO::PARAM_INT);
 $productFieldsQuery->execute();
 
+//change string of date to date format
+$date = date("Y-m-d H:i:s" , strtotime($date));
+
+$toReview = true;
+$queryRevision = "SELECT * FROM Revisions WHERE Revisions.product_category_id = " . $productCategoryId . " AND Revisions.company_id = " . $companyId . " AND Revisions.data = '" . $date . "' ;";
+$pre = $pdo->prepare($queryRevision);
+$pre->execute();
+$check = $pre->fetch(PDO::FETCH_ASSOC);
+if ($check) {
+    $toReview = false;
+}
+
 try {
   $pdo->beginTransaction();
   $pdo->query("INSERT INTO Sold_Products(`company_id`, `product_category_id`, `x`, `y`) VALUES (" . $companyId . ", " . $productCategoryId . ", " . $x . ", " . $y . ");");
@@ -23,7 +35,9 @@ try {
     );
     $pdo->query("INSERT INTO Product_Data(`sold_product_id`, `field_id`, `value`) VALUES (LAST_INSERT_ID(), '" . $row["field_id"] . "', '" . $_POST[$row["field_id"]] . "');");
   }
-  $pdo->query("INSERT INTO Revisions(`product_category_id`, `company_id`, `data`) VALUES (" . $productCategoryId . ", " . $companyId . ", '". $date . "');");
+  if ($toReview) {
+      $pdo->query("INSERT INTO Revisions(`product_category_id`, `company_id`, `data`) VALUES (" . $productCategoryId . ", " . $companyId . ", '". $date . "');");
+  }
   $pdo->commit();
 } catch (Exception $e) {
   echo $e;
